b59246b38c2741c311107bf8936d56644a73516d
Mahmoud Sameh <Mahmoudsameh25@gmail.com>
2025-01-15 16:08:46 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { ContentComponent } from '../content/content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
